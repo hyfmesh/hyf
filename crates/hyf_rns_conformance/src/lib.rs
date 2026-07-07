beaf10c8c7cb074acab467a057e1f30f38edb259
triesap <tyson@radroots.org>
2026-07-07 21:51:46 +0000

    chore(workspace): enforce lint and feature policy

--- crates/hyf_rns_conformance/src/lib.rs
+++ crates/hyf_rns_conformance/src/lib.rs
@@ -1,4 +1,9 @@
 #![forbid(unsafe_code)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::todo)]
+#![deny(clippy::unimplemented)]
+#![deny(clippy::unwrap_used)]
 
 #[cfg(test)]
 mod tests {
--- crates/hyf_rns_core/src/lib.rs
+++ crates/hyf_rns_core/src/lib.rs
@@ -1,4 +1,10 @@
+#![cfg_attr(not(any(test, feature = "std")), no_std)]
 #![forbid(unsafe_code)]
+#![deny(clippy::expect_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::todo)]
+#![deny(clippy::unimplemented)]
+#![deny(clippy::unwrap_used)]
 
 #[cfg(test)]
 mod tests {
