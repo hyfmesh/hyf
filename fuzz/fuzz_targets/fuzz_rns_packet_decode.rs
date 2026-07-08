fb126e2e362f15c33cbfa3653a439797e8f1d8de
triesap <tyson@radroots.org>
2026-07-08 05:34:58 +0000

    test(fuzz): seed rns parser corpus

--- fuzz/fuzz_targets/fuzz_rns_packet_decode.rs
+++ fuzz/fuzz_targets/fuzz_rns_packet_decode.rs
@@ -1,8 +1,14 @@
 #![no_main]
 
+mod seed_input;
+
+use hyf_rns_core::RNS_MTU;
 use hyf_rns_wire::decode_packet;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let _ = decode_packet(data);
+    let mut decoded = [0; RNS_MTU];
+    let input = seed_input::input_bytes(data, &mut decoded);
+
+    let _ = decode_packet(input);
 });
--- fuzz/fuzz_targets/fuzz_rns_packet_hash.rs
+++ fuzz/fuzz_targets/fuzz_rns_packet_hash.rs
@@ -1,13 +1,17 @@
 #![no_main]
 
+mod seed_input;
+
 use hyf_rns_core::RNS_MTU;
 use hyf_rns_wire::{packet_hash, packet_truncated_hash, write_packet_hashable_part};
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
+    let mut decoded = [0; RNS_MTU];
     let mut hashable = [0; RNS_MTU];
+    let input = seed_input::input_bytes(data, &mut decoded);
 
-    let _ = write_packet_hashable_part(data, &mut hashable);
-    let _ = packet_hash(data);
-    let _ = packet_truncated_hash(data);
+    let _ = write_packet_hashable_part(input, &mut hashable);
+    let _ = packet_hash(input);
+    let _ = packet_truncated_hash(input);
 });
